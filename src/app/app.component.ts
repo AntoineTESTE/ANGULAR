df518e146690288911749d760c4e131aab1778ca
Prestation LiveeDotCom <Prestation LiveeDotCom>
2017-06-13 18:06:19 +0200

    1er jour Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {  AppService } from './app.service';
 
+
+
 interface Animal {
   id: string;
   name: string;
 }
 
+
+// Le composant fait appel 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,9 +17,12 @@ interface Animal {
   providers: [AppService]
 })
 
+
+
 export class AppComponent {
   animals: Animal;
 
+// constructeur des services de l'API
   constructor(
     private appService: AppService
   ) { }

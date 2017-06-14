8233e90350e157d675adccef39139013283190a2
Prestation LiveeDotCom <Prestation LiveeDotCom>
2017-06-14 12:17:41 +0200

    new functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,33 +21,49 @@ export class AppComponent {
   animals: Animal;
   form = { name: '' };
 
-// constructeur des services de l'API
+  // constructeur des services de l'API
   constructor(
     private appService: AppService
   ) { }
 
+  // Fonction d'initialisation qui se lance au démarrage et lance la fonction Find
   ngOnInit() {
     this.appService.findAnimals()
       .subscribe(
-        (animals) => this.animals = animals,
-        (err) => console.error(err)
+      (animals) => this.animals = animals, // (animals) est ce qui est attendu en retour de fonction
+      (err) => console.error(err)
       );
   }
 
-  initForm(animal: any) {
-    if(animal) {
-      return this.form = animal;
+  // Fonction d'initialisation de formulaire (EDIT)
+  initForm(animal: any) { // initilisation du champ du formulaire avec un animal en paramètre
+    if (animal) { // si un animal est present
+      return this.form = Object.assign({}, animal); //retourne cet objet animal
     }
-    this.form = {
+    this.form = { // sinon tu renvoi un champ vide
       name: ''
     }
   }
 
-  removeAnimal(id: string) {
-    this.appService.removeAnimal(id)
+
+// Fonction de mise à jour de l'animal
+  update(form) { // si tu appui sur SEND tu envoie le formulaire
+    this.appService[form.id ? 'editAnimal' : 'createAnimal'](form) // si il n'a uniquement un id dans le formualaire tu envoie la fonction editAnimal sinon tu creer l'animal
       .subscribe(
-        () => this.ngOnInit(),
-        (err) => console.error(err)
+      () => this.ngOnInit(),
+      (err) => console.error(err)
       );
   }
+
+
+
+  //Fonction de suppresion de l'animal
+  removeAnimal(id: string) { // paramètre passé dans la fonction
+    this.appService.removeAnimal(id)
+      .subscribe(
+      () => this.ngOnInit(), // () indique si l'on attend quelque chose en retour de la fonction
+      (err) => console.error(err)
+      );
+  }
+
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+// Appel des routes de l'API
 @Injectable()
 export class AppService {
   baseUrl = 'http://localhost:8000';
@@ -12,11 +13,24 @@ export class AppService {
 
   findAnimals() {
     return this.http.get(`${this.baseUrl}/animals`)
-      .map(this.extractData);
+      .map(this.extractData); // si quelque chose est attendu en retour de la méthode appelé par la route
   }
 
   removeAnimal(id: string) {
     return this.http.delete(`${this.baseUrl}/animals/${id}`);
+  }
+
+
+// Route d'edition (l'ID inclus dans le formulaire = payload est envoyé)
+  editAnimal(payload) {
+    return this.http.put(`${this.baseUrl}/animals/${payload.id}`,{
+      name: payload.name
+    });
+  }
+
+// Route de creation (tout le formulaire = payload est envoyé avec ID et Nom)
+  createAnimal(payload) {
+    return this.http.post(`${this.baseUrl}/animals`,payload);
   }
 
   private extractData(res: Response) {
